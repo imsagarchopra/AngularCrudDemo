2eede432df72b58bcab9cb94cd5b1703c26066b0
Sagar Chopra <schopra@firemate.com>
2023-02-20 17:17:08 +0530

    Customise date picker

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule,Routes } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import {BsDatepickerModule} from 'ngx-bootstrap/datepicker'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -23,10 +24,11 @@ const appRoutes: Routes = [
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule,
     RouterModule.forRoot(appRoutes),
-    FormsModule,
-    BsDatepickerModule.forRoot()
+    BsDatepickerModule.forRoot(),
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/employees/create-employee.component.ts
+++ src/app/employees/create-employee.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import {NgForm} from '@angular/forms';
 import { Department } from '../models/department.model';
+import {BsDatepickerConfig} from 'ngx-bootstrap/datepicker';
 
 @Component({
   selector: 'app-create-employee',
@@ -9,12 +10,24 @@ import { Department } from '../models/department.model';
 })
 export class CreateEmployeeComponent {
 
+  datePickerConfig!: Partial<BsDatepickerConfig>;
   departments: Department[] =[
     {id: 1, name: 'Help Desk'},
     {id: 2, name: 'HR'},
     {id: 3, name: 'IT'},
     {id: 4, name: 'Payroll'},
   ];
+
+  constructor(){
+    this.datePickerConfig = Object.assign({},
+       {
+        containerClass: 'theme-dark-blue', 
+        showWeekNumbers: false,
+        minDate: new Date(2023,0,1),
+        maxDate: new Date(2023,11,31),
+        dateInputFormat: 'DD/MM/YYYY'
+       });
+  }
   saveEmployee(empForm: NgForm): void{
     console.log(empForm.value);
   }
