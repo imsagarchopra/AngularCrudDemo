e7448273d35501ee5fb195c88dcb10af3c53753d
Sagar Chopra <schopra@firemate.com>
2023-02-22 15:52:05 +0530

    custom validator

---
+++ src/app/shared/select-required-validator-directive.ts
@@ -0,0 +1,21 @@
+import { Directive } from "@angular/core";
+import { AbstractControl, NG_VALIDATORS, ValidationErrors, Validator } from "@angular/forms";
+
+@Directive({
+    selector:'[appSelectValidator]',
+    providers:[{
+        provide: NG_VALIDATORS,
+        useExisting: SelectRequiredValidatorDirective,
+        multi: true
+    }]
+})
+export class SelectRequiredValidatorDirective implements Validator
+{
+    validate(control: AbstractControl<any, any>): ValidationErrors | null {
+        return control.value === '-1' ? {'defaultSelected': true} : null;
+    }
+    registerOnValidatorChange?(fn: () => void): void {
+        throw new Error("Method not implemented.");
+    }
+
+}
