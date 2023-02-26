875e65720fd3ef1385561c31f4708459806f452f
Sagar Chopra <schopra@firemate.com>
2023-02-26 10:51:05 +0530

    Subscribe to route parameter changes

--- src/app/employees/employee-details.component.ts
+++ src/app/employees/employee-details.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Employee } from '../models/employee.model';
 import { EmployeeService } from './employee.service';
 
@@ -9,17 +9,32 @@ import { EmployeeService } from './employee.service';
   styleUrls: ['./employee-details.component.css']
 })
 export class EmployeeDetailsComponent implements OnInit {
-
-  employee!:Employee;
+  private _id!: number;
+  employee!: Employee;
 
   constructor(private _route: ActivatedRoute,
-    private _employeeService: EmployeeService){}
+    private _employeeService: EmployeeService,
+    private _router: Router) { }
 
   ngOnInit(): void {
-    const id =  +this._route.snapshot.params["id"];
-  //const id =  this._route.snapshot.paramMap.get('id');
+    //this._id =  +this._route.snapshot.params["id"];
+    //const id =  this._route.snapshot.paramMap.get('this._id');
 
-   this.employee = this._employeeService.getEmployee(id);
+    this._route.paramMap.subscribe(params => {
+      this._id = Number(params.get('id'));
+      this.employee = this._employeeService.getEmployee(this._id);
+    });
   }
 
+  viewNextEmployee(): void {
+
+    if(this._id < 3)
+    {
+      this._id = this._id + 1;
+    }
+    else{
+      this._id = 1;
+    }
+    this._router.navigate(['/employees', this._id]);
+  }
 }
